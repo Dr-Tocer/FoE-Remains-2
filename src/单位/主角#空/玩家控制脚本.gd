aa6e17b5ec6dfff606cf11f8040b601be96ea060
Dr-Tocer <2270236832@qq.com>
2025-03-28 17:13:43 +0800

    准备构建状态机

--- src/单位/主角#空/玩家控制脚本.gd
+++ src/单位/主角#空/玩家控制脚本.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 @onready var animation_player = $AnimationPlayer#获取角色动画节点
+@onready var 行走碰撞箱 = $"行走碰撞箱"
+@onready var 爬行碰撞箱 = $"爬行碰撞箱"
 enum State { 向右休息,向右行走,向右跳跃,向右上行,向右下行,向右爬行,向右奔跑,向右站立,向右翻滚,向右站立持枪,向右攀爬,向右攀附,向右坠落,向左休息,向左行走,向左跳跃,向左上行,向左下行,向左爬行,向左奔跑,向左站立,向左翻滚,向左站立持枪,向左攀爬,向左坠落,向左攀附}# 角色行动状态组
 var current_state#玩家当前状态
 @export var speed:float = 400 #基础移动速度
 @export var 奔跑增加速度:float = 400
 @export var acceleration:float = 30 #移动加速度
-@export var jump_velocity:float = -1100 #跳跃最大速度
-@export var G_coefficient_when_drop:float = 2.6 #角色重力加速度
-@export var 跳跃蓄力速度:float = 150
+@export var jump_velocity:float = -1000 #跳跃最大速度
+@export var G_coefficient_when_drop:float = 2.2 #角色重力加速度
+@export var 跳跃蓄力速度:float = 75
 var G = ProjectSettings.get_setting("physics/2d/default_gravity")
 var 实际速度:float = speed
 var is_dropping:bool #是否在下落
@@ -16,12 +18,17 @@ var 跳跃蓄力值:float
 var 跳跃就绪状态:bool = true
 var 攀爬状态 : bool = false
 var 攀爬跳跃就绪:bool = false
+var 爬行状态 :bool = false
 var 朝向 : bool = true #true为右，false为左
 var 左上 : bool #角色附近区域障碍物检测，有为true，无为false
 var 左侧 : bool
 var 右上 : bool
 var 右侧 : bool
 var 下方 : bool
+var 腿部右上 : bool
+var 腿部右侧 : bool
+var 腿部左上 : bool
+var 腿部左侧 : bool
 func _ready() -> void:
 	pass
 func _physics_process(delta):
@@ -36,41 +43,37 @@ func _physics_process(delta):
 	else :
 		攀爬状态 = false
 		is_dropping = false
+	if current_state != State.向右爬行 and current_state != State.向左爬行:
+		爬行状态 = false
+		$"行走碰撞箱".disabled = false
+		$"爬行碰撞箱".disabled = true
 	move_and_slide()
-#-----------------攀附系统-------------------
+#-----------------攀附系统---------------------
 func  攀爬系统():
 	if Input.is_action_pressed("A") and current_state == State.向右攀附 and not is_on_floor() and not 攀爬状态 and 朝向:
-		var X :int
+		var X :int = $"右侧区域".get_collision_point().x
 		var Y :int
 		if $"右侧区域".get_collision_point().y > 0 :
-			X = $"右侧区域".get_collision_point().x
 			Y = $"右侧区域".get_collision_point().y/40
 			Y = (Y)*40
-		elif $"右侧区域".get_collision_point().y < 0 :
-			X = $"右侧区域".get_collision_point().x
+		else:
 			Y = ($"右侧区域".get_collision_point().y)/40
 			Y = (Y-1)*40
-
-		self.global_transform.origin = Vector2(X-130,Y-70)
+		self.global_transform.origin = Vector2(X-46,Y+28)
 		animation_player.play("向右攀附")
 		攀爬状态 = true
-		
-	if Input.is_action_pressed("A") and current_state == State.向左攀附 and not is_on_floor() and not 攀爬状态 and not 朝向:
-		var X :int
+	elif Input.is_action_pressed("A") and current_state == State.向左攀附 and not is_on_floor() and not 攀爬状态 and not 朝向:
+		var X :int = $"左侧区域".get_collision_point().x
 		var Y :int
 		if $"左侧区域".get_collision_point().y > 0 :
-			X = $"左侧区域".get_collision_point().x
 			Y = $"左侧区域".get_collision_point().y/40
 			Y = (Y)*40
-		elif $"左侧区域".get_collision_point().y < 0 :
-			X = $"左侧区域".get_collision_point().x
+		else:
 			Y = ($"左侧区域".get_collision_point().y)/40
 			Y = (Y-1)*40
-
-		self.global_transform.origin = Vector2(X-48,Y-70)
+		self.global_transform.origin = Vector2(X+46,Y+28)
 		animation_player.play("向左攀附")
 		攀爬状态 = true
-		
 		pass
 	if 攀爬状态:
 		velocity = Vector2.ZERO
@@ -85,7 +88,7 @@ func  攀爬系统():
 			G = ProjectSettings.get_setting("physics/2d/default_gravity")
 			if Input.is_action_pressed("左摇杆向左"):
 				velocity.x = -speed
-			if Input.is_action_pressed("左摇杆向右"):
+			elif Input.is_action_pressed("左摇杆向右"):
 				velocity.x = speed
 		elif Input.is_action_just_released("A"):
 			攀爬跳跃就绪 = true
@@ -93,6 +96,11 @@ func  攀爬系统():
 			攀爬状态 = false
 			攀爬跳跃就绪 = false
 			G = ProjectSettings.get_setting("physics/2d/default_gravity")
+		elif Input.is_action_just_pressed("左摇杆向下"):
+			攀爬状态 = false
+			攀爬跳跃就绪 = false
+			G = ProjectSettings.get_setting("physics/2d/default_gravity")
+			
 		pass
 	右上 = $"右上区域".is_colliding()
 	右侧 = $"右侧区域".is_colliding()
@@ -103,7 +111,7 @@ func  攀爬系统():
 	#####
 	#print(攀爬跳跃就绪)
 	#####
-#------------------移动系统------------------------
+#-----------------移动系统---------------------
 func 移动系统():
 	var direction = Input.get_axis("左摇杆向左","左摇杆向右")
 	if direction != 0 :#角色移动速度从当前速度通过加速度到目标速度
@@ -126,8 +134,42 @@ func 移动系统():
 	if Input.is_action_just_released("左摇杆按压") and 奔跑状态:
 		实际速度 -= 奔跑增加速度
 		奔跑状态 = false
-	pass
-#---------------重力系统-----------------
+	#----------跨上一格高区块----------
+	腿部右上 = $"腿部右上区域".is_colliding()
+	腿部右侧 = $"腿部右侧区域".is_colliding()
+	腿部左上 = $"腿部左上区域".is_colliding()
+	腿部左侧 = $"腿部左侧区域".is_colliding()
+	if not 腿部右上 and 腿部右侧 and 朝向:
+		var Y :int
+		if $"腿部右侧区域".get_collision_point().y > 0 :
+			Y = $"腿部右侧区域".get_collision_point().y/40
+			Y = (Y)*40
+		else:
+			Y = ($"腿部右侧区域".get_collision_point().y)/40
+			Y = (Y-1)*40
+		self.global_transform.origin.y += Y - $"腿部右侧区域".get_collision_point().y -11
+		velocity.x = 0.8*实际速度
+		velocity.y = 0
+	elif not 腿部左上 and 腿部左侧 and not 朝向:
+		var Y :int
+		if $"腿部左侧区域".get_collision_point().y > 0 :
+			Y = $"腿部左侧区域".get_collision_point().y/40
+			Y = (Y)*40
+		else:
+			Y = ($"腿部左侧区域".get_collision_point().y)/40
+			Y = (Y-1)*40
+		self.global_transform.origin.y += Y - $"腿部左侧区域".get_collision_point().y -11
+		velocity.x = -0.8*实际速度
+		velocity.y = 0
+	if Input.is_action_just_pressed("B"):
+		$"爬伏触发计时器".start(0.3)
+	if Input.is_action_just_released("B"):
+		if $"爬伏触发计时器".time_left > 0:
+			$"爬伏触发计时器".stop()
+			print("掩护")
+			#到时候写进入掩体代码
+			pass
+#-----------------重力系统---------------------
 func 重力系统(delta):
 	if velocity.y <= 0:
 		is_dropping = false
@@ -137,33 +179,38 @@ func 重力系统(delta):
 		pass
 	velocity.y += G * G_coefficient_when_drop * delta
 	#print(G * G_coefficient_when_drop * delta)
-#-------------------跳跃系统------------------------
+#-----------------跳跃系统---------------------
 func 跳跃系统():
 	if Input.get_action_strength("A") != 0 and is_on_floor() and 跳跃就绪状态:
 		跳跃蓄力值 = move_toward(跳跃蓄力值,jump_velocity,跳跃蓄力速度)
 	if Input.is_action_just_released("A") and not is_dropping and is_on_floor():
-		velocity.y = 跳跃蓄力值
-		跳跃蓄力值 = 0
-		跳跃就绪状态 = false
+		if 跳跃蓄力值 < 0.6 * jump_velocity:
+			velocity.y = 跳跃蓄力值
+			跳跃蓄力值 = 0
+			跳跃就绪状态 = false
+		else :
+			velocity.y = 0.6 * jump_velocity
+			跳跃蓄力值 = 0
+			跳跃就绪状态 = false
 	if Input.is_action_just_released("A"): #松开跳跃键才能再次跳跃
 		跳跃就绪状态 = true
 	#if is_dropping and 跳跃蓄力值 < -跳跃蓄力速度: #蓄力时离开边缘自动跳跃
 		#velocity.y = 跳跃蓄力值
 		#跳跃蓄力值 = 0
 		#跳跃就绪状态 = false
-#--------------按键与状态绑定-------------------
+#---------------按键与状态绑定------------------
 func 按键与状态绑定():
 	if Input.is_action_pressed("左摇杆向右"):
 		朝向 = true
 	elif Input.is_action_pressed("左摇杆向左"):
 		朝向 = false
-	if Input.is_action_pressed("左摇杆向右") and is_on_floor():
+	if Input.is_action_pressed("左摇杆向右") and is_on_floor() and not 爬行状态:
 		current_state = State.向右行走
-	elif Input.is_action_pressed("左摇杆向左") and is_on_floor():
+	elif Input.is_action_pressed("左摇杆向左") and is_on_floor() and not 爬行状态:
 		current_state = State.向左行走
-	elif Input.is_action_pressed("左摇杆按压") and Input.is_action_pressed("左摇杆向左") and is_on_floor():
+	elif Input.is_action_pressed("左摇杆按压") and Input.is_action_pressed("左摇杆向左") and is_on_floor() and not 爬行状态:
 		current_state = State.向左奔跑
-	elif Input.is_action_pressed("左摇杆按压") and Input.is_action_pressed("左摇杆向右") and is_on_floor():
+	elif Input.is_action_pressed("左摇杆按压") and Input.is_action_pressed("左摇杆向右") and is_on_floor() and not 爬行状态:
 		current_state = State.向右奔跑
 	elif 朝向 and not is_on_floor() and not is_dropping and not 攀爬状态 and velocity.y < -500:
 		current_state = State.向右跳跃
@@ -178,11 +225,11 @@ func 按键与状态绑定():
 	elif is_dropping and not 朝向 and not 攀爬状态 and velocity.y > 400 and not 下方:
 		current_state = State.向左坠落
 	else:
-		if 朝向 and is_on_floor():
+		if 朝向 and is_on_floor() and not 爬行状态:
 			current_state = State.向右休息
-		elif not 朝向 and is_on_floor():
+		elif not 朝向 and is_on_floor() and not 爬行状态:
 			current_state = State.向左休息
-#---------------状态与动画绑定--------------------
+#---------------状态与动画绑定------------------
 func 状态与动画绑定():
 	match current_state:
 		State.向右休息:
@@ -201,4 +248,28 @@ func 状态与动画绑定():
 			animation_player.play("向右坠落")
 		State.向左坠落:
 			animation_player.play("向左坠落")
+		State.向右爬行:
+			animation_player.play("向右爬行")
+		State.向左爬行:
+			animation_player.play("向左爬行")
 		pass
+
+
+
+func _on_爬伏触发计时器_timeout() -> void:
+	if 爬行状态 == false:
+		爬行状态 = true
+		$"行走碰撞箱".disabled = true
+		$"爬行碰撞箱".disabled = false
+	else :
+		爬行状态 = false
+	if 朝向 :
+		current_state = State.向右爬行
+		
+	else :
+		current_state = State.向左爬行
+		
+	
+	
+	print("爬")
+	pass # Replace with function body.
--- src/单位/主角#空/移动系统的碰撞箱.gd
+++
@@ -1,7 +0,0 @@
-extends CollisionPolygon2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	add_to_group("player_hitboxes")
-	pass # Replace with function body.
