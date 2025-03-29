0a531c175b98b18de05bc1c420dd0f82eed9ca33
Dr-Tocer <2270236832@qq.com>
2025-03-29 18:18:28 +0800

    原主角模板作废

--- src/单位/状态机/待机.gd
+++ src/单位/状态机/待机.gd
@@ -1,6 +1,6 @@
 extends Node
 class_name 待机
-@onready var 玩家 = get_parent().get_parent() if get_parent() else null
+@onready var entity = get_parent().get_parent() if get_parent() else null
 @onready var 状态机 = get_parent()
 
 func _physics_process(delta: float) -> void:
@@ -16,5 +16,17 @@ func 退出状态():
 	pass
 	
 func 更新进程():
+	if entity and entity is CharacterBody2D :
+		玩家进入状态条件()
+	else:
+		NPC进入状态条件()
+		pass
+	pass
+	
+func 玩家进入状态条件():
+	if entity.is_on_floor() and 状态机.当前状态 != "匍匐":
+		状态机.变更状态 = name
+	pass
+func NPC进入状态条件():
 	
 	pass
